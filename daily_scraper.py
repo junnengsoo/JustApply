f26168e4aff7abbb6b0786b8f2ba4be49436a241
junnengsoo <soojunneng01@gmail.com>
2025-08-16 14:01:47 +0800

    🔄 Daily scrape update: Removed outdated job URLs from the scraper list.

--- daily_scraper.py
+++ daily_scraper.py
@@ -238,9 +238,6 @@ def main():
         "https://stripe.com/jobs/search?office_locations=Asia+Pacific--Singapore&tags=University",
         "https://www.google.com/about/careers/applications/jobs/results/?src=Online/Google%20Website/ByF&distance=50&employment_type=INTERN&company=Fitbit&company=Google&location=Singapore&location=London,%20UK",
         "https://www.metacareers.com/jobs?roles%5B0%5D=Internship&offices%5B0%5D=London%252C%2520UK&offices%5B1%5D=Singapore&offices%5B2%5D=Menlo%2520Park%252C%2520CA&offices%5B3%5D=New%2520York%252C%2520NY&offices%5B4%5D=Seattle%252C%2520WA",
-        "https://jobs.apple.com/en-sg/search?location=singapore-SGP+london-LND+united-states-USA&team=internships-STDNT-INTRN",
-        "https://www.squarepoint-capital.com/open-opportunities?id=6212106",
-        "https://www.jumptrading.com/careers/?locations=Singapore+New-York+Sydney+Chicago+Hong-Kong+London&titleSearch=campus+intern",
         "https://www.drw.com/work-at-drw/listings?filterType=keyword&value=software",
         "https://caladan.xyz/careers/",
         "https://grasshopperasia.com/job/trading/",
@@ -249,7 +246,6 @@ def main():
         "https://www.quantedge.com/careers",
         "https://careers.twosigma.com/careers/OpenRoles/?5081=%5B16718737%5D&5081_format=3146&listFilterMode=1&jobRecordsPerPage=10&",
         "https://careers.point72.com/?experience=internships&location=singapore;hong%20kong;sydney",
-        "https://careers.thetradedesk.com/jobs?category=interns"
     ]
 
     process_urls(urls)
